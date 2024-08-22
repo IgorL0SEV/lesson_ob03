4d066f609450cd85b134a322b50342ca07799430
IgorL0SEV <igor.v.losev@gmail.com>
2024-08-22 20:49:54 +0300

    class Animal

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+
+
+# 1. Создайте базовый класс `Animal`, который будет содержать общие атрибуты
+# (например, `name`, `age`) и методы (`make_sound()`, `eat()`) для всех животных.
+
+class Animal():
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def make_sound(self):
+    return f"{self.name} - издаёт звуки"
+
+  def eat(self):
+    return f"{self.name} - кушает"
+
