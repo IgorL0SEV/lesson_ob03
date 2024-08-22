d073564a690d303e1ffcda1ae06c78ed626784ee
IgorL0SEV <igor.v.losev@gmail.com>
2024-08-22 20:53:18 +0300

    sub_class Animal

--- main.py
+++ main.py
@@ -15,3 +15,37 @@ class Animal():
   def eat(self):
     return f"{self.name} - кушает"
 
+# 2. Реализуйте наследование, создав подклассы `Bird`, `Mammal`, и `Reptile`,
+# которые наследуют от класса `Animal`. # Добавьте специфические атрибуты и
+# переопределите методы, если требуется (например, различный звук для `make_sound()`).
+
+class Bird(Animal):
+  def __init__(self, name, age):
+    super().__init__(name, age)
+
+  def make_sound(self):
+    return f"{self.name} - чирикает"
+
+  def eat(self):
+    return f"{self.name} - клюёт корм"
+
+class Mammal(Animal):
+  def __init__(self, name, age):
+    super().__init__(name,age)
+
+  def make_sound(self):
+    return f"{self.name} - ревёт"
+
+  def eat(self):
+    return f"{self.name} - жуёт корм"
+
+class Reptile(Animal):
+  def __init__(self, name, age):
+    super().__init__(name, age)
+
+  def make_sound(self):
+    return f"{self.name} - шипит"
+
+  def eat(self):
+    return f"{self.name} - заглатывает корм"
+
